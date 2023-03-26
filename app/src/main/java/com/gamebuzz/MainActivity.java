095f4f7f95da7f5bb71c3e13335af701ee22b467
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-03-26 16:19:55 +0200

    Starting GameBuzz v2

--- app/src/main/java/com/gamebuzz/MainActivity.java
+++ app/src/main/java/com/gamebuzz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gamebuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
 }
