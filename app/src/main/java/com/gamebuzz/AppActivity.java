96601fa7f6929c7a9b5eb10f5c0b292f8e30d6ce
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-04-06 12:50:34 +0200

    navigation AppActivity

---
+++ app/src/main/java/com/gamebuzz/AppActivity.java
@@ -0,0 +1,31 @@
+package com.gamebuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.searchFragment, R.id.exploreFragment).build();
+
+        // For the BottomNavigationView
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+}
