9037348c12762f88cd9bc7579ce964b17aaac797
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-04-06 10:59:57 +0200

    starting bottom navbar

--- app/src/main/java/com/gamebuzz/MainActivity.java
+++ app/src/main/java/com/gamebuzz/MainActivity.java
@@ -1,16 +1,20 @@
 package com.gamebuzz;
-
+import java.io.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.gamebuzz.HomeFragment;
+import com.gamebuzz.SearchFragment;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_home);
 
+        NavHostFragment navHostFragment = ()
     }
-
 }
