ca2f558a39db5669b1695f378595c944baf39de3
alemoscardo <alemw2@gmail.com>
2023-07-12 21:35:11 +0200

    Fix crash del signup IDtoken

--- app/src/main/java/com/gamebuzz/ui/welcome/SignupFragment.java
+++ app/src/main/java/com/gamebuzz/ui/welcome/SignupFragment.java
@@ -2,7 +2,11 @@ package com.gamebuzz.ui.welcome;
 
 import android.os.Bundle;
 
+import static com.gamebuzz.util.Constants.EMAIL_ADDRESS;
+import static com.gamebuzz.util.Constants.ENCRYPTED_SHARED_PREFERENCES_FILE_NAME;
+import static com.gamebuzz.util.Constants.ID_TOKEN;
 import static com.gamebuzz.util.Constants.MINIMUM_PASSWORD_LENGTH;
+import static com.gamebuzz.util.Constants.PASSWORD;
 import static com.gamebuzz.util.Constants.USER_COLLISION_ERROR;
 import static com.gamebuzz.util.Constants.WEAK_PASSWORD_ERROR;
 
@@ -30,8 +34,12 @@ import com.gamebuzz.data.repository.user.IUserRepository;
 import com.gamebuzz.databinding.FragmentSignupBinding;
 import com.gamebuzz.model.Result;
 import com.gamebuzz.model.User;
+import com.gamebuzz.util.DataEncryptionUtil;
 import com.gamebuzz.util.ServiceLocator;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class SignupFragment extends Fragment {
 
@@ -39,7 +47,7 @@ public class SignupFragment extends Fragment {
 
     private FragmentSignupBinding binding;
     private UserViewModel userViewModel;
-    // TODO: private DataEncryptionUtil dataEncryptionUtil
+    private DataEncryptionUtil dataEncryptionUtil;
 
     public SignupFragment() {
         // Required empty public constructor
@@ -49,11 +57,12 @@ public class SignupFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
         IUserRepository userRepository = ServiceLocator.getInstance().getUserRepository(requireActivity().getApplication());
         userViewModel = new ViewModelProvider(requireActivity(), new UserViewModelFactory(userRepository)).get(UserViewModel.class);
         userViewModel.setAuthenticationError(false);
-        // TODO: dataEncryptionUtil = new DataEncryptionUtil(requireActivity().getApplication();
+        dataEncryptionUtil = new DataEncryptionUtil(requireActivity().getApplication());
     }
 
     @Override
@@ -95,7 +104,7 @@ public class SignupFragment extends Fragment {
                                 Log.e(TAG, "Observe working");
                                 if(result.isSuccess()) {
                                     User user = ((Result.UserResponseSuccess) result).getData();
-                                    // TODO: saveLoginData(email, password, user.getIdToken());
+                                    saveLoginData(email, password, user.getIdToken());
                                     userViewModel.setAuthenticationError(false);
                                     Navigation.findNavController(view).navigate(
                                             R.id.navigate_to_appActivity
@@ -157,6 +166,17 @@ public class SignupFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void saveLoginData(String email, String password, String idToken) {
+        try {
+            dataEncryptionUtil.writeSecretDaatWithEncryptedSharedPreferences(ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, EMAIL_ADDRESS, email);
+            dataEncryptionUtil.writeSecretDaatWithEncryptedSharedPreferences(ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, PASSWORD, password);
+            dataEncryptionUtil.writeSecretDaatWithEncryptedSharedPreferences(ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, ID_TOKEN, idToken);
+
+        } catch (GeneralSecurityException | IOException e ) {
+            e.printStackTrace();
+        }
     }
 
 }
